a2576eefe6ed0865af4fa52fd89984b8ce678725
Kim <kimharris2018@gmail.com>
2023-07-03 01:31:09 -0500

    __init__.py update, forms, api routes

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.card_routes import cards
+from .api.board_routes import board_routes
+from .api.list_routes import lists
 from .seeds import seed_commands
 from .config import Config
 
@@ -30,6 +32,8 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(cards, url_prefix='/api/cards')
+app.register_blueprint(board_routes, url_prefix='/api/boards')
+app.register_blueprint(lists, url_prefix='/api/lists')
 db.init_app(app)
 Migrate(app, db)
 
