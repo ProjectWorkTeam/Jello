2ca14ecae90a40e5ee514204c271d91625fc9c0c
Peter <guan_peter123@yahoo.com>
2023-07-06 10:16:27 -0700

    landing

--- react-app/src/components/BoardModal/BoardModal.js
+++ react-app/src/components/BoardModal/BoardModal.js
@@ -1,3 +1,98 @@
+// import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { useHistory } from 'react-router-dom';
+// import { thunkAddBoard } from '../../store/boardsReducer';
+// import image1 from '../../assets/image1.jpg';
+// import image2 from '../../assets/image2.jpg';
+// import image3 from '../../assets/image3.jpg';
+// import './boardModal.css';
+
+// const BoardModal = ({ closeModal }) => {
+//   const [boardName, setBoardName] = useState('');
+//   const [selectedImage, setSelectedImage] = useState('');
+
+//   const dispatch = useDispatch();
+//   const history = useHistory(); // Hook to manage navigation
+
+//   const handleImageSelect = (image) => {
+//     setSelectedImage(image);
+//   };
+
+//   const handleBoardNameChange = (e) => {
+//     setBoardName(e.target.value);
+//   };
+
+//   const handleCreateBoard = async () => {
+//     if (boardName.trim() === '' || selectedImage === '') {
+//       // Handle error case
+//       return;
+//     }
+
+//     const newBoard = {
+//       name: boardName,
+//       image: selectedImage,
+//     };
+
+//     const createdBoard = await dispatch(thunkAddBoard(newBoard)); // Dispatch the thunkAddBoard action with the new board details
+//     if (createdBoard) {
+//       closeModal();
+//       history.push(`/boards/${createdBoard.id}`); // Navigate to the new board's page
+//     }
+//   };
+
+//   return (
+//     <div id="modal">
+//       <div id="modal-background" onClick={closeModal} />
+//       <div id="modal-content">
+//         <h2>Create a Board</h2>
+//         <div>
+//           <input
+//             type="text"
+//             placeholder="Enter board name"
+//             value={boardName}
+//             onChange={handleBoardNameChange}
+//           />
+//         </div>
+//         <div>
+//           <h3>Choose a Background Image</h3>
+//           <div>
+//             <img
+//               src={image1}
+//               alt="background1"
+//               className="background-img"
+//               onClick={() => handleImageSelect(image1)}
+//             />
+//             <img
+//               src={image2}
+//               alt="background2"
+//               className="background-img"
+//               onClick={() => handleImageSelect(image2)}
+//             />
+//             <img
+//               src={image3}
+//               alt="background3"
+//               className="background-img"
+//               onClick={() => handleImageSelect(image3)}
+//             />
+//           </div>
+//         </div>
+//         <div>
+//           <h3>Selected Image Preview</h3>
+//           {selectedImage && (
+//             <img src={selectedImage} className="background-img" alt="Selected background" />
+//           )}
+//         </div>
+//         <div>
+//           <button onClick={handleCreateBoard}>Create</button>
+//           <button onClick={closeModal}>Cancel</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default BoardModal;
+
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
@@ -9,7 +104,7 @@ import './boardModal.css';
 
 const BoardModal = ({ closeModal }) => {
   const [boardName, setBoardName] = useState('');
-  const [selectedImage, setSelectedImage] = useState('');
+  const [selectedImage, setSelectedImage] = useState(image1); // Set image1 as the default selected image
 
   const dispatch = useDispatch();
   const history = useHistory(); // Hook to manage navigation
@@ -58,20 +153,20 @@ const BoardModal = ({ closeModal }) => {
           <div>
             <img
               src={image1}
-              alt="Image 1"
-              className="background-img"
+              alt="background1"
+              className={`modal-background-img ${selectedImage === image1 ? 'selected' : ''}`}
               onClick={() => handleImageSelect(image1)}
             />
             <img
               src={image2}
-              alt="Image 2"
-              className="background-img"
+              alt="background2"
+              className={`modal-background-img ${selectedImage === image2 ? 'selected' : ''}`}
               onClick={() => handleImageSelect(image2)}
             />
             <img
               src={image3}
-              alt="Image 3"
-              className="background-img"
+              alt="background3"
+              className={`modal-background-img ${selectedImage === image3 ? 'selected' : ''}`}
               onClick={() => handleImageSelect(image3)}
             />
           </div>
@@ -79,7 +174,7 @@ const BoardModal = ({ closeModal }) => {
         <div>
           <h3>Selected Image Preview</h3>
           {selectedImage && (
-            <img src={selectedImage} className="background-img" alt="Selected Image" />
+            <img src={selectedImage} className="modal-background-img" alt="Selected background" />
           )}
         </div>
         <div>
--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { login } from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
+import jello from "../../assets/Jello.jpg";
 import './LoginForm.css';
 
 function LoginFormPage() {
@@ -10,6 +11,7 @@ function LoginFormPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
+  const history = useHistory();
 
   if (sessionUser) return <Redirect to="/" />;
 
@@ -21,36 +23,64 @@ function LoginFormPage() {
     }
   };
 
+  const demo = async () => {
+    const data = await dispatch(login('demo@aa.io', 'password'))
+    if (data) {
+      setErrors(data);
+    }
+  }
+
+  const handleLogoClick = () => {
+    history.push("/");
+  };
+
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
+    <div className="container">
+      <div className="top">
+        <div className="logo-container" onClick={handleLogoClick}>
+          <div className="logo-wrapper">
+            <img src={jello} alt="Jello" className="logo-image" />
+          </div>
+          <div className="logo-text">Jello</div>
+        </div>
+      </div>
+      <h1 className="input-title">Log In</h1>
+      <form onSubmit={handleSubmit} className="form-wrapper">
         <ul>
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
           ))}
         </ul>
-        <label>
-          Email
+        <div className="form-group">
+          {/* <label>Email</label> */}
           <input
             type="text"
             value={email}
+            placeholder="Email"
+            className="inputs"
             onChange={(e) => setEmail(e.target.value)}
             required
           />
-        </label>
-        <label>
-          Password
+        </div>
+        <div className="form-group">
+          {/* <label>Password</label> */}
           <input
             type="password"
             value={password}
+            placeholder="Password"
+            className="inputs"
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-        </label>
-        <button type="submit">Log In</button>
+        </div>
+        <button className="buttons" type="submit">Log In</button>
       </form>
-    </>
+      <div className="demo-wrapper">
+        <button className="buttons" onClick={demo}>
+          Demo User
+        </button>
+      </div>
+    </div>
   );
 }
 
