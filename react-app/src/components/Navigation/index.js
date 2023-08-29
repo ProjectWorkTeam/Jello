e353a611b4d6cfc79425c1bc12088cb8727df5e7
Peter <guan_peter123@yahoo.com>
2023-08-28 18:40:41 -0700

    added contacts

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -3,27 +3,36 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import Contact from "../Contact/index"
+import OpenModalButton from '../OpenModalButton';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-	  <header>
-		<nav className='nav-container'>
-		  <ul className="nav-contents">
-			<li className="home-link">
-			  <NavLink exact to="/">Jello</NavLink>
-			</li>
-			{isLoaded && (
-			  <li id="profile-wrapper">
-				<ProfileButton user={sessionUser} />
-			  </li>
-			)}
-		  </ul>
-		</nav>
-	  </header>
+		<header>
+			<nav className='nav-container'>
+				<ul className="nav-contents">
+					<li className="home-link">
+						<NavLink exact to="/">Jello</NavLink>
+					</li>
+					<li className='nav-contact'>
+						<OpenModalButton
+							className="contact-us"
+							buttonText="Contact Us"
+							modalComponent={<Contact />}
+						/>
+					</li>
+					{isLoaded && (
+						<li id="profile-wrapper">
+							<ProfileButton user={sessionUser} />
+						</li>
+					)}
+				</ul>
+			</nav>
+		</header>
 	);
-  }
+}
 
 
 export default Navigation;
